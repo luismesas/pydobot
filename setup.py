314a1b8d5994f0c76c50da8623a756e06e1d56f9
Keko <luismesas@gmail.com>
2017-04-07 20:33:27 -0700

    replace build num variable on setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-import os
 from distutils.core import setup
-
-build_num = os.environ['CIRCLE_BUILD_NUM']
 
 setup(
     name='pydobot',
     packages=['pydobot'],
-    version='0.2.%s' % build_num,
+    version='0.2.${CIRCLE_BUILD_NUM}',
     description='Python 3 library for Dobot Magician',
     author='Luis Mesas',
     author_email='luismesas@gmail.com',
     url='https://github.com/luismesas/pydobot',
-    download_url='https://github.com/luismesas/pydobot/archive/0.2.%s.tar.gz' % build_num,
+    download_url='https://github.com/luismesas/pydobot/archive/0.2.${CIRCLE_BUILD_NUM}.tar.gz',
     keywords=['dobot', 'magician', 'robotics'],
     classifiers=[],
     install_requires=[
