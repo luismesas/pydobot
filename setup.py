e513cb2931a21a22f58f5dbb99474672868f1550
Keko <luismesas@gmail.com>
2017-04-07 18:26:17 -0700

    clean pyPip setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='pydobot',
-    packages=['pydobot'],  # this must be the same as the name above
+    packages=['pydobot'],
     version='0.1',
     description='Python 3 library for Dobot Magician',
     author='Luis Mesas',
     author_email='luismesas@gmail.com',
-    url='https://github.com/luismesas/pydobot',  # use the URL to the github repo
-    download_url='https://github.com/luismesas/pydobot/archive/0.1.tar.gz',  # I'll explain this in a second
-    keywords=['dobot', 'magician', 'robotics'],  # arbitrary keywords
+    url='https://github.com/luismesas/pydobot',
+    download_url='https://github.com/luismesas/pydobot/archive/0.1.tar.gz',
+    keywords=['dobot', 'magician', 'robotics'],
     classifiers=[],
 )
