f52f1ab20a241012cc8967e3fcea43f1a459454c
Keko <luismesas@gmail.com>
2017-04-07 19:29:13 -0700

    set CI build number as revision vesion number

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import os
 from distutils.core import setup
+
+build_num = os.environ['CIRCLECI_BUILD_NUM']
 
 setup(
     name='pydobot',
     packages=['pydobot'],
-    version='0.2',
+    version='0.2.%s' % build_num,
     description='Python 3 library for Dobot Magician',
     author='Luis Mesas',
     author_email='luismesas@gmail.com',
     url='https://github.com/luismesas/pydobot',
-    download_url='https://github.com/luismesas/pydobot/archive/0.2.tar.gz',
+    download_url='https://github.com/luismesas/pydobot/archive/0.2.%s.tar.gz' % build_num,
     keywords=['dobot', 'magician', 'robotics'],
     classifiers=[],
+    install_requires=['pyserial']
 )
