2cc88c8c60d9888a92467fac7f31048750237bcc
Keko <luismesas@gmail.com>
2017-04-07 18:33:41 -0700

    allow to set a verbose mode to print debug information

--- pydobot/dobot.py
+++ pydobot/dobot.py
@@ -33,8 +33,9 @@ class Dobot(threading.Thread):
 
     # joint_angles = [4]
 
-    def __init__(self, port):
+    def __init__(self, port, verbose=False):
         threading.Thread.__init__(self)
+        self.verbose = verbose
         self.lock = threading.Lock()
         self.ser = serial.Serial(port,
                                  baudrate=115200,
@@ -42,50 +43,52 @@ class Dobot(threading.Thread):
                                  stopbits=serial.STOPBITS_ONE,
                                  bytesize=serial.EIGHTBITS)
         is_open = self.ser.isOpen()
-        print('%s open' % self.ser.name if is_open else 'failed to open serial port')
+        if self.verbose:
+            print('pydobot: %s open' % self.ser.name if is_open else 'failed to open serial port')
         self._set_ptp_coordinate_params(velocity=200.0, acceleration=200.0)
         self._set_ptp_common_params(velocity=200.0, acceleration=200.0)
         self.start()
 
     def run(self):
         while self.on:
-            self._get_pose(verbose=False)
+            self._get_pose()
             time.sleep(0.2)
 
     def close(self):
         self.on = False
         self.lock.acquire()
         self.ser.close()
-        print('%s closed' % self.ser.name)
+        if self.verbose:
+            print('pydobot: %s closed' % self.ser.name)
         self.lock.release()
 
-    def _send_command(self, msg, verbose=True):
+    def _send_command(self, msg):
         self.lock.acquire()
-        self._send_message(msg, verbose)
-        response = self._read_message(verbose)
+        self._send_message(msg)
+        response = self._read_message()
         self.lock.release()
         return response
 
-    def _send_message(self, msg, verbose=True):
+    def _send_message(self, msg):
         time.sleep(0.1)
-        if verbose:
-            print('>>', msg)
+        if self.verbose:
+            print('pydobot: >>', msg)
         self.ser.write(msg.bytes())
 
-    def _read_message(self, verbose=True):
+    def _read_message(self):
         time.sleep(0.1)
         b = self.ser.read_all()
         if len(b) > 0:
             msg = Message(b)
-            if verbose:
-                print('<<', msg)
+            if self.verbose:
+                print('pydobot: <<', msg)
             return msg
         return
 
-    def _get_pose(self, verbose=True):
+    def _get_pose(self):
         msg = Message()
         msg.id = ID_GET_POSE
-        response = self._send_command(msg, verbose)
+        response = self._send_command(msg)
         if response is not None:
             if response.id == ID_GET_POSE:
                 self.x = struct.unpack_from('f', response.params, 0)[0]
@@ -96,11 +99,12 @@ class Dobot(threading.Thread):
                 self.j2 = struct.unpack_from('f', response.params, 20)[0]
                 self.j3 = struct.unpack_from('f', response.params, 24)[0]
                 self.j4 = struct.unpack_from('f', response.params, 28)[0]
-        # print("x:%3.1f y:%3.1f z:%3.1f r:%3.1f j1:%3.1f j2:%3.1f j3:%3.1f j4:%3.1f" % (
-        #     self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4))
+        if self.verbose:
+            print("pydobot: x:%03.1f y:%03.1f z:%03.1f r:%03.1f j1:%03.1f j2:%03.1f j3:%03.1f j4:%03.1f" %
+                  (self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4))
         return response
 
-    def _set_cp_cmd(self, x, y, z, verbose=True):
+    def _set_cp_cmd(self, x, y, z):
         msg = Message()
         msg.id = ID_SET_CP_CMD
         msg.ctrl = 0x03
@@ -109,9 +113,9 @@ class Dobot(threading.Thread):
         msg.params.extend(bytearray(struct.pack('f', y)))
         msg.params.extend(bytearray(struct.pack('f', z)))
         msg.params.append(0x00)
-        return self._send_command(msg, verbose)
+        return self._send_command(msg)
 
-    def _set_ptp_coordinate_params(self, velocity, acceleration, verbose=True):
+    def _set_ptp_coordinate_params(self, velocity, acceleration):
         msg = Message()
         msg.id = 81
         msg.ctrl = 0x03
@@ -120,18 +124,18 @@ class Dobot(threading.Thread):
         msg.params.extend(bytearray(struct.pack('f', velocity)))
         msg.params.extend(bytearray(struct.pack('f', acceleration)))
         msg.params.extend(bytearray(struct.pack('f', acceleration)))
-        return self._send_command(msg, verbose)
+        return self._send_command(msg)
 
-    def _set_ptp_common_params(self, velocity, acceleration, verbose=True):
+    def _set_ptp_common_params(self, velocity, acceleration):
         msg = Message()
         msg.id = 83
         msg.ctrl = 0x03
         msg.params = bytearray([])
         msg.params.extend(bytearray(struct.pack('f', velocity)))
         msg.params.extend(bytearray(struct.pack('f', acceleration)))
-        return self._send_command(msg, verbose)
+        return self._send_command(msg)
 
-    def _set_ptp_cmd(self, x, y, z, r, mode, verbose=True):
+    def _set_ptp_cmd(self, x, y, z, r, mode):
         msg = Message()
         msg.id = 84
         msg.ctrl = 0x03
@@ -141,9 +145,9 @@ class Dobot(threading.Thread):
         msg.params.extend(bytearray(struct.pack('f', y)))
         msg.params.extend(bytearray(struct.pack('f', z)))
         msg.params.extend(bytearray(struct.pack('f', r)))
-        return self._send_command(msg, verbose)
+        return self._send_command(msg)
 
-    def _set_end_effector_suction_cup(self, suck=False, verbose=True):
+    def _set_end_effector_suction_cup(self, suck=False):
         msg = Message()
         msg.id = 62
         msg.ctrl = 0x03
@@ -153,7 +157,7 @@ class Dobot(threading.Thread):
             msg.params.extend(bytearray([0x01]))
         else:
             msg.params.extend(bytearray([0x00]))
-        return self._send_command(msg, verbose)
+        return self._send_command(msg)
 
     def go(self, x, y, z, r=0.):
         self._set_ptp_cmd(x, y, z, r, mode=MODE_PTP_MOVJ_XYZ)
