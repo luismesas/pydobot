80ac463abc9a7ea33ca6cc8aa7ed5ea0c8b8f732
Keko <luismesas@gmail.com>
2017-04-07 19:13:18 -0700

    increase to version 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pydobot',
     packages=['pydobot'],
-    version='0.1',
+    version='0.2',
     description='Python 3 library for Dobot Magician',
     author='Luis Mesas',
     author_email='luismesas@gmail.com',
     url='https://github.com/luismesas/pydobot',
-    download_url='https://github.com/luismesas/pydobot/archive/0.1.tar.gz',
+    download_url='https://github.com/luismesas/pydobot/archive/0.2.tar.gz',
     keywords=['dobot', 'magician', 'robotics'],
     classifiers=[],
 )
