49169af0edb6317b5933dc104108cff7701d442e
Chaz Selph <csselph@gmail.com>
2020-02-08 11:24:23 -0700

    Remove redundant comment

--- pydobot/enums/ptpMode.py
+++ pydobot/enums/ptpMode.py
@@ -1,47 +1,52 @@
 from enum import Enum
 
 
-# ptpMode: int // PTP mode (Value range 0-9)
-# x, y, z, r: float
-# x,y,z,r are the parameters of ptpMode, which can be set as coordinates, joint angles, or angle increments
-
 class PTPMode(Enum):
     """
     0. JUMP_XYZ, ]
         Jump mode,
         (x,y,z,r)
         is the target point in Cartesian coordinate system
+
     1. MOVJ_XYZ,
         Joint movement,
         (x,y,z,r)
         is the target point in Cartesian coordinate system
+
     2. MOVL_XYZ,
         Linear movement,
         (x,y,z,r)
         is the target point in Cartesian coordinate system
+
     3. JUMP_ANGLE,
         Jump mode, (x,y,z,r)
         is the target point in Jointcoordinate system
+
     4. MOVJ_ANGLE,
         Joint movement,
         (x,y,z,r)
         is the target point in Joint coordinate system
+
     5. MOVL_ANGLE,
         Linear movement,
         (x,y,z,r)
         is the target point in Joint coordinate system
+
     6. MOVJ_INC,
         Joint movement increment mode,
         (x,y,z,r)
         is the angle increment in Joint coordinate system
+
     7. MOVL_INC,
         Linear movement increment mode,
         (x,y,z,r)
         is the Cartesian coordinate increment in Joint coordinate system
+
     8. MOVJ_XYZ_INC,
         Joint movement increment mode,
         (x,y,z,r)
         is the Cartesian coordinate increment in Cartesian coordinate system
+
     9. JUMP_MOVL_XYZ,
         Jump movement,
         (x,y,z,r)
